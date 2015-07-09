21692602bacfa85310a56a927670212bd5335f4c
Shravan <shravan.kasagoni@outlook.com>
2015-07-09 18:47:33 +0530

    JSON Date Filter
    JSON Date Filter exmaple

---
+++ JSONDateFilter/script.js
@@ -0,0 +1,10 @@
+var jsonDateConverterExample= angular.module('jsonDateConverterExample', [])
+  .controller('JsonDateController', ['$scope', function($scope) {
+    $scope.sampleJsonDate = '\/Date(1433913313004-0800)\/';
+}]);
+
+jsonDateConverterExample.filter('jsonDate', ['$filter', function ($filter) {
+    return function (input, format) {
+        return (input) ? $filter('date')(parseInt(input.substr(6)), format) : '';
+    };
+}]);
