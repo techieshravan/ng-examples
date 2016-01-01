dd8296c7a246e3c7e4ece4bc65b00cb9731c5abb
Shravan <shravan.kasagoni@outlook.com>
2016-01-01 10:03:05 +0530

    Angular2

---
+++ Angular2/Example1/app.ts
@@ -0,0 +1,11 @@
+import {Component, bootstrap} from 'angular2/angular2';
+
+@Component({
+	selector: 'my-app',
+	template: '<h1>My First Angular 2 App'
+})
+class AppComponent {
+
+}
+
+bootstrap(AppComponent);
---
+++ Angular2/README.md
@@ -0,0 +1,2 @@
+# Angular2
+Angular 2 Examples
