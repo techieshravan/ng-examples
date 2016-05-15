524c169c82d4c6ea6bbe56586c2a21371822144e
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-05-16 00:19:28 +0530

    Quick Start App

---
+++ material2-examples/example1/app/app.component.ts
@@ -0,0 +1,10 @@
+import {Component} from '@angular/core';
+// import {MdToolbar} from '@angular2-material/toolbar';
+// import {MdButton} from '@angular2-material/button';
+
+@Component({
+  selector: 'material-app',
+  templateUrl: '/app/app.component.html'
+  //,directives: [MdToolbar, MdButton]
+})
+export class AppComponent {}
---
+++ quickstart/app/app.component.ts
@@ -0,0 +1,9 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'quick-start-app',
+  templateUrl: '/app/app.component.html'
+})
+export class AppComponent {
+  message:string = 'Angualr 2 Quick Start';
+}
