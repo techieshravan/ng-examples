6c4ee0d09bcfa3bcfb70eb8ae592186015123be9
Shravan Kumar Kasagoni <shravan.kasagoni@outlook.com>
2016-05-26 02:06:02 +0530

    added some logic to calculate emi

--- emi-calculator/src/app/emi-calculator.component.ts
+++ emi-calculator/src/app/emi-calculator.component.ts
@@ -7,5 +7,27 @@ import { Component } from '@angular/core';
   styleUrls: ['emi-calculator.component.css']
 })
 export class EmiCalculatorAppComponent {
-  title = 'emi-calculator works test!';
+
+  title:string = 'EMI Calculator';
+  loanAmount: number = 1000000;
+  interestRate: number = 10.5;
+  loanTenure: number = 120;
+  emi: number;
+
+  calculateEmi() {
+
+    if(this.loanAmount && this.interestRate && this.loanTenure) {
+
+
+      let interestPerMonth = this.interestRate/12/100;
+
+      let numerator = Math.pow((1+interestPerMonth), this.loanTenure);
+
+      let denominator = numerator - 1;
+
+      this.emi = (this.loanAmount * interestPerMonth) * numerator/denominator;
+
+      console.log(this.emi);
+    }
+  }
 }
